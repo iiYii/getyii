30c6f79ba694ed68f3e98e3a06222605ee2f7a51
forecho <caizhenghai@gmail.com>
2014-12-26 12:57:14 +0800

    first commit

---
+++ common/config/db.php
@@ -0,0 +1,15 @@
+<?php
+return [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=getyii', // MySQL, MariaDB
+            //'dsn' => 'sqlite:/path/to/database/file', // SQLite
+            //'dsn' => 'pgsql:host=localhost;port=5432;dbname=mydatabase', // PostgreSQL
+            //'dsn' => 'cubrid:dbname=demodb;host=localhost;port=33000', // CUBRID
+            //'dsn' => 'sqlsrv:Server=localhost;Database=mydatabase', // MS SQL Server, sqlsrv driver
+            //'dsn' => 'dblib:host=localhost;dbname=mydatabase', // MS SQL Server, dblib driver
+            //'dsn' => 'mssql:host=localhost;dbname=mydatabase', // MS SQL Server, mssql driver
+            //'dsn' => 'oci:dbname=//localhost:1521/mydatabase', // Oracle
+            'username' => 'root',
+            'password' => '123',
+            'charset' => 'utf8',
+];

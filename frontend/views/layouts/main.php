a3e40d58333fb8986fb18bf8d0f519786808ecf6
蔡正海 <caizhenghai@gmail.com>
2014-12-31 13:34:33 +0800

    Revert "Revert "添加博客详情静态页面:smile:""
    This reverts commit 9f1136c806d0a49bc17a70aa72dd69a970eae738.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -20,6 +20,15 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
 </head>
 <body>
 <?php $this->beginBody() ?>

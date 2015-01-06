436116bbca6839eaa9166ed114446faf11926f74
forecho <caizhenghai@gmail.com>
2015-01-06 18:34:41 +0800

    文章首页右边栏独立成一个文件

--- frontend/views/post/index.php
+++ frontend/views/post/index.php
@@ -12,84 +12,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <section id="blog" class="container">
     <div class="row">
-        <aside class="col-sm-4 col-sm-push-8">
-            <div class="widget search">
-                <?php $form = ActiveForm::begin([
-                    'action' => ['index'],
-                    'method' => 'get',
-                    'fieldConfig' => [
-                        'template' => "{input}\n{hint}"
-                    ]
-                ]); ?>
-                    <div class="input-group">
-                        <?= $form->field($searchModel, 'title')->textInput([
-                            'class' => 'form-control',
-                            'autocomplete' => 'off',
-                            'placeholder' => 'Search'
-                        ]) ?>
-                        <span class="input-group-btn">
-                            <?= Html::submitButton('<i class="icon-search"></i>', ['class' => 'btn btn-danger']) ?>
-                        </span>
-                    </div>
-                <?php ActiveForm::end(); ?>
-            </div><!--/.search-->
+        <?= $this->render('_right',[
+            'model' => $searchModel,
+            'category' => $category,
+            'tags' => $tags,
+        ]); ?>
 
-            <div class="widget ads">
-                <div class="row">
-                    <div class="col-xs-6">
-                        <a href="#"><img class="img-responsive img-rounded" src="images/ads/ad1.png" alt=""></a>
-                    </div>
-
-                    <div class="col-xs-6">
-                        <a href="#"><img class="img-responsive img-rounded" src="images/ads/ad2.png" alt=""></a>
-                    </div>
-                </div>
-                <p> </p>
-                <div class="row">
-                    <div class="col-xs-6">
-                        <a href="#"><img class="img-responsive img-rounded" src="images/ads/ad3.png" alt=""></a>
-                    </div>
-
-                    <div class="col-xs-6">
-                        <a href="#"><img class="img-responsive img-rounded" src="images/ads/ad4.png" alt=""></a>
-                    </div>
-                </div>
-            </div><!--/.ads-->
-
-            <div class="widget categories">
-                <h3>Blog Categories</h3>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <ul class="arrow">
-                            <?php foreach ($category as $key => $value): ?>
-                                <li><?= Html::a(Html::encode($value->name), ['/post/index', 'PostSearch[post_meta_id]' => $value->id]);?></li>
-                            <?php endforeach ?>
-                        </ul>
-                    </div>
-                    <!-- <div class="col-sm-6">
-                        <ul class="arrow">
-                            <li><a href="#">Joomla</a></li>
-                            <li><a href="#">Wordpress</a></li>
-                            <li><a href="#">Drupal</a></li>
-                            <li><a href="#">Magento</a></li>
-                            <li><a href="#">Bootstrap</a></li>
-                        </ul>
-                    </div> -->
-                </div>
-            </div><!--/.categories-->
-            <div class="widget tags">
-                <h3>Tag Cloud</h3>
-                <ul class="tag-cloud">
-                    <?php foreach ($tags as $key => $value): ?>
-                        <li><?= Html::a(
-                            Html::encode($value->name),
-                            ['/post/index', 'PostSearch[post_meta_id]' => $value->id],
-                            ['class' => 'btn btn-xs btn-primary']
-                        );?></li>
-                    <?php endforeach ?>
-                </ul>
-            </div><!--/.tags-->
-        </aside>
         <div class="col-sm-8 col-sm-pull-4">
             <div class="blog">
                 <?= ListView::widget([
--- frontend/views/post/view.php
+++ frontend/views/post/view.php
@@ -10,6 +10,130 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Posts', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
+<section id="blog" class="container">
+    <div class="row">
+        <?= $this->render('_right',[
+            'model' => $searchModel,
+            'category' => $category,
+            'tags' => $tags,
+        ]); ?>
+
+        <div class="col-sm-8 col-sm-pull-4">
+            <div class="blog">
+                <div class="blog-item">
+                    <img class="img-responsive img-blog" src="images/blog/blog2.jpg" width="100%" alt="" />
+                    <div class="blog-content">
+                        <h3>Duis sed odio sit amet nibh vulputate cursus</h3>
+                        <div class="entry-meta">
+                            <span><i class="icon-user"></i> <a href="#">John</a></span>
+                            <span><i class="icon-folder-close"></i> <a href="#">Bootstrap</a></span>
+                            <span><i class="icon-calendar"></i> Sept 16th, 2012</span>
+                            <span><i class="icon-comment"></i> <a href="blog-item.html#comments">3 Comments</a></span>
+                        </div>
+                        <p class="lead">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+
+                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+
+                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+
+                        <hr>
+
+                        <div class="tags">
+                            <i class="icon-tags"></i> Tags <a class="btn btn-xs btn-primary" href="#">CSS3</a> <a class="btn btn-xs btn-primary" href="#">HTML5</a> <a class="btn btn-xs btn-primary" href="#">WordPress</a> <a class="btn btn-xs btn-primary" href="#">Joomla</a>
+                        </div>
+
+                        <p>&nbsp;</p>
+
+                        <div class="author well">
+                            <div class="media">
+                                <div class="pull-left">
+                                    <img class="avatar img-thumbnail" src="images/blog/avatar.jpg" alt="">
+                                </div>
+                                <div class="media-body">
+                                    <div class="media-heading">
+                                        <strong>John Doe</strong>
+                                    </div>
+                                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
+                                </div>
+                            </div>
+                        </div><!--/.author-->
+
+                        <div id="comments">
+                            <div id="comments-list">
+                                <h3>3 Comments</h3>
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img class="avatar img-circle" src="images/blog/avatar1.png" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <div class="well">
+                                            <div class="media-heading">
+                                                <strong>John Doe</strong>&nbsp; <small>27 Aug 2013</small>
+                                                <a class="pull-right" href="#"><i class="icon-repeat"></i> Reply</a>
+                                            </div>
+                                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                                        </div>
+                                        <div class="media">
+                                            <div class="pull-left">
+                                                <img class="avatar img-circle" src="images/blog/avatar3.png" alt="">
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="well">
+                                                    <div class="media-heading">
+                                                        <strong>John Doe</strong>&nbsp; <small>27 Aug 2013</small>
+                                                        <a class="pull-right" href="#"><i class="icon-repeat"></i> Reply</a>
+                                                    </div>
+                                                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+                                                </div>
+                                            </div>
+                                        </div><!--/.media-->
+                                    </div>
+                                </div><!--/.media-->
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img class="avatar img-circle" src="images/blog/avatar2.png" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <div class="well">
+                                            <div class="media-heading">
+                                                <strong>John Doe</strong>&nbsp; <small>27 Aug 2013</small>
+                                                <a class="pull-right" href="#"><i class="icon-repeat"></i> Reply</a>
+                                            </div>
+                                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                                        </div>
+                                    </div>
+                                </div><!--/.media-->
+                            </div><!--/#comments-list-->
+
+                            <div id="comment-form">
+                                <h3>Leave a comment</h3>
+                                <form class="form-horizontal" role="form">
+                                    <div class="form-group">
+                                        <div class="col-sm-6">
+                                            <input type="text" class="form-control" placeholder="Name">
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <input type="email" class="form-control" placeholder="Email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-12">
+                                            <textarea rows="8" class="form-control" placeholder="Comment"></textarea>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-danger btn-lg">Submit Comment</button>
+                                </form>
+                            </div><!--/#comment-form-->
+                        </div><!--/#comments-->
+                    </div>
+                </div><!--/.blog-item-->
+            </div>
+        </div><!--/.col-md-8-->
+    </div><!--/.row-->
+</section><!--/#blog-->
+
+
 <section id="blog" class="container">
     <div class="post-view">
 

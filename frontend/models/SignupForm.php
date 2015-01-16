7766df29568a2b82483641f2e8beebc72859593c
forecho <caizhenghai@gmail.com>
2015-01-16 16:20:19 +0800

    注册使用事物 注册时添加用户信息

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -2,6 +2,7 @@
 namespace frontend\models;
 
 use common\models\User;
+use common\models\UserInfo;
 use yii\base\Model;
 use Yii;
 
@@ -43,12 +44,30 @@ class SignupForm extends Model
     public function signup()
     {
         if ($this->validate()) {
-            $user = new User();
-            $user->username = $this->username;
-            $user->email = $this->email;
-            $user->setPassword($this->password);
-            $user->generateAuthKey();
-            $user->save();
+            // 启用事物
+            $transaction=\Yii::$app->db->beginTransaction();
+                $user = new User();
+                $user->username = $this->username;
+                $user->email = $this->email;
+                $user->setPassword($this->password);
+                $user->generateAuthKey();
+                $return = $user->save();
+
+                // 添加用户信息
+                $userInfo = new UserInfo();
+                $userInfo->user_id = $user->id;
+                $userInfo->prev_login_time = time();
+                $userInfo->prev_login_ip = Yii::$app->getRequest()->getUserIP();
+                $userInfo->last_login_time = time();
+                $userInfo->last_login_ip = Yii::$app->getRequest()->getUserIP();
+                $userInfo->created_at = time();
+                $userInfo->updated_at = time();
+                $return = $userInfo->save();
+            if(!$return){
+                $transaction->rollback();
+            }else{
+                $transaction->commit();
+            }
             return $user;
         }
 

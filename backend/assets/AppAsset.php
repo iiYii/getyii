50704159c967e7e69c4b09a5618a143152c85e51
forecho <caizhenghai@gmail.com>
2015-01-20 15:01:50 +0800

    使用sb-admin-2的bower

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -19,10 +19,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/sb-admin-2.css',
     ];
     public $js = [
-        'js/sb-admin-2.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- backend/assets/BackendAsset.php
+++ backend/assets/BackendAsset.php
@@ -10,10 +10,12 @@ class BackendAsset extends AssetBundle
 {
     public $sourcePath = '@bower';
     public $css = [
+        'sb-admin-2-assets/dist/css/sb-admin-2.css',
         'font-awesome/css/font-awesome.min.css',
         'metisMenu/dist/metisMenu.min.css',
     ];
     public $js = [
         'metisMenu/dist/metisMenu.min.js',
+        'sb-admin-2-assets/dist/js/sb-admin-2.js',
     ];
 }

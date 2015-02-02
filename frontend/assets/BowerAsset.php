c1f6f49b8006eb2ca40a3a546a5d3494d4ff3d8b
forecho <caizhenghai@gmail.com>
2015-02-02 11:41:39 +0800

    添加bower资源管理文件

---
+++ frontend/assets/BowerAsset.php
@@ -0,0 +1,17 @@
+<?php
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author forecho <caizhenghai@gmail.com>
+ */
+class BowerAsset extends AssetBundle
+{
+    public $sourcePath = '@bower';
+    public $css = [
+        'font-awesome/css/font-awesome.min.css',
+    ];
+    public $js = [
+    ];
+}

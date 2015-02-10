fc5eaff6f94c3f31f37138a5cbf5ea4ad0e90569
forecho <caizhenghai@gmail.com>
2015-02-10 18:03:32 +0800

    个人主页换结构

--- frontend/modules/user/views/default/show.php
+++ frontend/modules/user/views/default/show.php
@@ -9,71 +9,154 @@ use yii\widgets\ListView;
 $username = Yii::$app->getRequest()->getQueryParam('username');
 ?>
 <section class="container user-default-index">
-    <div class="col-xs-12 col-sm-6 col-md-6">
-        <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <img src="http://gravatar.com/avatar/<?= md5($user->email) ?>?s=230" alt="" class="img-rounded img-responsive" />
+
+    <div class="col-sm-3">
+        <div class="panel panel-default thumbnail center">
+            <br>
+            <img src="http://gravatar.com/avatar/<?= md5($user->email) ?>?s=230" alt="用户头像" title="用户头像" class="img-circle img-responsive" />
+            <h1 class=""><?= Html::tag('strong', $user->username) ?></h1>
+            <!-- <button type="button" class="btn btn-success">Book me!</button> -->
+            <!-- <button type="button" class="btn btn-info">Send me a message</button> -->
+            <!-- <br> -->
+        </div>
+
+        <!--left col-->
+        <ul class="list-group">
+            <li class="list-group-item text-muted" contenteditable="false">Profile</li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Joined</strong></span>
+                2.13.2014
+            </li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Last seen</strong></span>
+                Yesterday
+            </li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Real name</strong></span>
+                Joseph
+                Doe
+            </li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Role: </strong></span> Pet
+                Sitter
+            </li>
+        </ul>
+        <div class="panel panel-default">
+            <div class="panel-heading">Insured / Bonded?
+
             </div>
-            <div class="col-sm-6 col-md-8">
-                <h4><?= Html::tag('strong', $user->username), '，', $user->tagline ?></h4>
-                <ul style="padding: 0; list-style: none outside none;">
+            <div class="panel-body"><i style="color:green" class="fa fa-check-square"></i> Yes, I am insured and bonded.
 
-                    <?php if (!empty($user->userInfo->location)): ?>
-                        <li><i class="fa fa-map-marker text-muted"></i> <?= Html::encode($user->userInfo->location) ?></li>
-                    <?php endif; ?>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">Website <i class="fa fa-link fa-1x"></i>
 
-                    <?php if (!empty($user->userInfo->company)): ?>
-                        <li><i class="fa fa-laptop"></i> <?= Html::a(Html::encode($user->userInfo->company), Html::encode($user->userInfo->company)) ?></li>
-                    <?php endif; ?>
+            </div>
+            <div class="panel-body"><a href="http://bootply.com" class="">bootply.com</a>
 
-                    <?php if (!empty($user->userInfo->website)): ?>
-                        <li><i class="fa fa-globe text-muted"></i> <?= Html::a(Html::encode($user->userInfo->website), Html::encode($user->userInfo->website)) ?></li>
-                    <?php endif; ?>
+            </div>
+        </div>
 
-                    <?php if (!empty($user->userInfo->github)): ?>
-                        <li><i class="fa fa-github text-muted"></i> <?= Html::a(Html::encode($user->userInfo->github), Html::encode($user->userInfo->github)) ?></li>
-                    <?php endif; ?>
+        <ul class="list-group">
+            <li class="list-group-item text-muted">Activity <i class="fa fa-dashboard fa-1x"></i>
 
-                    <li><i class="fa fa-calendar text-muted"></i> <?= '加入于 ', Yii::$app->formatter->asDateTime($user->userInfo->created_at) ?></li>
+            </li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Shares</strong></span> 125
+            </li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Likes</strong></span> 13
+            </li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Posts</strong></span> 37
+            </li>
+            <li class="list-group-item text-right"><span class="pull-left"><strong class="">Followers</strong></span> 78
+            </li>
+        </ul>
+        <div class="panel panel-default">
+            <div class="panel-heading">Social Media</div>
+            <div class="panel-body"><i class="fa fa-facebook fa-2x"></i> <i class="fa fa-github fa-2x"></i>
+                <i class="fa fa-twitter fa-2x"></i> <i class="fa fa-pinterest fa-2x"></i> <i
+                    class="fa fa-google-plus fa-2x"></i>
 
-                    <?php if (!empty($user->userInfo->info)): ?>
-                        <li><i class="fa fa-user text-muted"></i> <?= Html::a(Html::encode($user->userInfo->info), Html::encode($user->userInfo->info)) ?></li>
-                    <?php endif; ?>
-
-                </ul>
-                <?php if (!empty($user->userInfo->bio)): ?>
-                    <p><?= Html::encode($user->userInfo->bio) ?></p>
-                <?php endif; ?>
             </div>
         </div>
     </div>
+    <!--/col-3-->
+    <div class="col-sm-9" contenteditable="false" style="">
+        <div class="panel panel-default">
+            <div class="panel-heading">Starfox221's Bio</div>
+            <div class="panel-body"> A long description about me.
 
+            </div>
+        </div>
+        <div class="panel panel-default target">
+            <div class="panel-heading" contenteditable="false">Pets I Own</div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <img alt="300x200" src="http://lorempixel.com/600/200/people">
 
-    <div class="col-md-9">
-        </br>
-        <?= Menu::widget([
-            'options' => [
-                'class' => 'nav nav-tabs nav-justified'
-            ],
-            'items' => [
-                ['label' => '最新评论',  'url' => ['/user/default/show', 'username'=> $username]],
-                ['label' => '最新主题',  'url' => ['/user/default/post', 'username'=> $username]],
-                ['label' => '最新收藏',  'url' => ['/user/default/favorite', 'username'=> $username]],
-            ]
-        ]) ?>
+                            <div class="caption">
+                                <h3>
+                                    Rover
+                                </h3>
 
-        <div class="list-group">
-            <?= ListView::widget([
-                'dataProvider' => $dataProvider,
-                'itemOptions' => ['class' => 'item'],
-                'summary' => false,
-                'itemView' => '_view',
-                'pager' => [
-                    'options' => ['class'=>'pagination pagination-lg'],
-                    'prevPageLabel' => '<i class="icon-angle-left"></i>',
-                    'nextPageLabel' => '<i class="icon-angle-right"></i>',
-                ]
-            ]) ?>
+                                <p>
+                                    Cocker Spaniel who loves treats.
+                                </p>
+
+                                <p>
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <img alt="300x200" src="http://lorempixel.com/600/200/city">
+
+                            <div class="caption">
+                                <h3>
+                                    Marmaduke
+                                </h3>
+
+                                <p>
+                                    Is just another friendly dog.
+                                </p>
+
+                                <p>
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="thumbnail">
+                            <img alt="300x200" src="http://lorempixel.com/600/200/sports">
+
+                            <div class="caption">
+                                <h3>
+                                    Rocky
+                                </h3>
+
+                                <p>
+                                    Loves catnip and naps. Not fond of children.
+                                </p>
+
+                                <p>
+
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">Starfox221's Bio</div>
+            <div class="panel-body"> A long description about me.
+
+            </div>
         </div>
     </div>
 </section>

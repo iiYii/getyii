37040036e926cb4f610b1f8d6d6b2097b8e67b50
forecho <caizhenghai@gmail.com>
2015-02-14 15:42:37 +0800

    前台话题分类添加所有话题

--- frontend/views/topic/index.php
+++ frontend/views/topic/index.php
@@ -16,31 +16,34 @@ use common\models\PostMeta;
 
     <div class="col-sm-12 list-nav" contenteditable="false" style="">
         <nav class="navbar navbar-default">
-        <?= Nav::widget([
-            'options' => [
-                'class' => 'nav navbar-nav',
-            ],
-            'items' => [
-                ['label' => '社区',  'url' => false],
-            ]
-        ]) ?>
-        <?php foreach (PostMeta::topic() as $key => $value) {
-            $navItems[] = ['label' => $value, 'url' => ['/topic/index', 'type' => $key]];
-        } ?>
-        <?= Nav::widget([
-            'options' => [
-                'class' => 'nav navbar-nav navbar-right',
-            ],
-            'items' => [
-                [
-                    'label' => '全部话题',
-                    'items' => $navItems,
+        <?php
+            echo Nav::widget([
+                'options' => [
+                    'class' => 'nav navbar-nav',
                 ],
-                ['label' => '最新主题',  'url' => ['/topic/index', 'tab' => 'newest']],
-                ['label' => '热门主题',  'url' => ['/topic/index', 'tab' => 'hot']],
-                ['label' => '+ 新话题',  'url' => ['/topic/create'], 'options' => ['class' => 'mr15', 'id' => 'new-topic']],
-            ]
-        ]) ?>
+                'items' => [
+                    ['label' => '社区',  'url' => false],
+                ]
+            ]);
+            $navItems[] = ['label' => '全部话题', 'url' => ['/topic/index', 'tag' => 'index']];
+            foreach (PostMeta::topic() as $key => $value) {
+                $navItems[] = ['label' => $value, 'url' => ['/topic/index', 'tag' => $key]];
+            }
+            echo Nav::widget([
+                'options' => [
+                    'class' => 'nav navbar-nav navbar-right',
+                ],
+                'items' => [
+                    [
+                        'label' => '全部话题',
+                        'items' => $navItems,
+                    ],
+                    ['label' => '最新主题',  'url' => ['/topic/index', 'tab' => 'newest']],
+                    ['label' => '热门主题',  'url' => ['/topic/index', 'tab' => 'hot']],
+                    ['label' => '+ 新话题',  'url' => ['/topic/create'], 'options' => ['class' => 'mr15', 'id' => 'new-topic']],
+                ]
+            ])
+        ?>
         </nav>
 
         <?= ListView::widget([

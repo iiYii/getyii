fb814b3b784b1a70a4d8084528ab50d4e706cf20
forecho <caizhenghai@gmail.com>
2015-03-21 17:26:18 +0800

    用户名只能为字母和数字进行注册

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -23,6 +23,7 @@ class SignupForm extends Model
         return [
             ['username', 'filter', 'filter' => 'trim'],
             ['username', 'required'],
+            ['username', 'match', 'pattern' => '/^[a-z]\w*$/i', 'message' => '{attribute}只能为数字和字母'],
             ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => '此{attribute}已经被使用'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 

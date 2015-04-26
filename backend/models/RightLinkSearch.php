f3a8a29d2c69ac350489ea36d72e797d88a6fb99
forecho <caizhenghai@gmail.com>
2015-04-26 17:19:45 +0800

    解决文件名错误的 bug

--- backend/models/RightLinkSearch.php
+++ backend/models/RightLinkSearch.php
@@ -50,8 +50,6 @@ class RightLinkSearch extends RightLink
         $this->load($params);
 
         if (!$this->validate()) {
-            // uncomment the following line if you do not want to any records when validation fails
-            // $query->where('0=1');
             return $dataProvider;
         }
 

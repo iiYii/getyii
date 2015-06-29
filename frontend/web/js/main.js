f0c6f842ae61efa3ec63f866f823a10cefb227e9
60 <1096364408@qq.com>
2015-06-30 01:20:22 +0800

    submit失败时button不设置为disabled

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -209,7 +209,7 @@ jQuery(function ($) {
     }
 
     $('form').submit(function () {
-        $(this).find("button[type='submit']").prop('disabled', true);
+        //$(this).find("button[type='submit']").prop('disabled', true);
     });
 
     //add by ruzuojun

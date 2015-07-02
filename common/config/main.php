852a516f3bab872a0cc51a47d2fca8229aa66209
ruzuojun <ruzuojun@139.com>
2015-07-02 15:42:18 +0800

    将session数据存入数据库

--- common/config/main.php
+++ common/config/main.php
@@ -22,5 +22,10 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'session' => [
+            'class' => 'yii\web\DbSession',
+            // 'db' => 'mydb',  // 数据库连接的应用组件ID，默认为'db'.
+            'sessionTable' => 'session', // session 数据表名，默认为'session'.
+        ],
     ],
 ];

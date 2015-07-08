0f284e6c3eef84a728a38c3278439c5e2aedc448
xiao tu zi <sunjie20081001@126.com>
2015-07-08 14:51:59 +0800

    全部删除

--- common/components/AssetBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace common\components;
-
-class AssetBundle extends \yii\web\AssetBundle
-{
-    public $publishOptions = [
-        'forceCopy' => YII_DEBUG // debug模式时强制拷贝
-    ];
-}
--- frontend/web/js/nav.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ruzuojun on 2015/6/26.
- */
-
-$(".pinned").pin({containerSelector: ".container", minWidth: 940});
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})

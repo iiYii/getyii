ec8aa7f1da34897d6220b071ab9b6bfb14bf0368
forecho <caizhenghai@gmail.com>
2015-08-06 15:22:52 +0800

    简单调整下后台用户页面和文章分类页面

--- backend/views/post-meta/index.php
+++ backend/views/post-meta/index.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="post-meta-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Create Post Meta', ['create'], ['class' => 'btn btn-success']) ?>
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-//        'filterModel' => $searchModel,
+        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 

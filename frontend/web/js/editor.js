36ffab4f402d711c054963f5e3b8327164976c79
forecho <caizhenghai@gmail.com>
2015-12-09 07:32:11 +0800

    往话题编辑器里面插入代码模版 ok

--- frontend/web/js/editor.js
+++ frontend/web/js/editor.js
@@ -1,10 +1,10 @@
 $('.insert-codes a').click(function (e) {
     e.preventDefault();
     var editor = ace.edit("markdown");
-    var source = editor.getSession().getValue();
+    var source = editor.getValue();
     var language = $(this).data('lang');
     var prefixBreak = source ? "\n" : '';
     var srcMerged = prefixBreak + "```" + language + "\n\n```\n";
-    console.log(source);
-    editor.getSession().setValue(source + srcMerged);
+    editor.insert(srcMerged);
+    editor.gotoLine(editor.getCursorPosition().row -1);
 });

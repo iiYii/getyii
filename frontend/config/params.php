406ce5595846998b96a6062d802ddf3edb4a83db
ruzuojun <ruzuojun@139.com>
2016-03-26 16:16:53 +0800

    Merge branch 'master' into dbachina.com
    Conflicts:
    	frontend/views/layouts/main.php

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,6 +3,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'donateNode' => ['mysql','oracle','mongodb'], //开启打赏节点
     'setting' => [
-        'xunsearch' => true, // true 表示开启 GetYii xunsearch 搜索功能，默认不开启
+        'xunsearch' => false, // true 表示开启 GetYii xunsearch 搜索功能，默认不开启
     ],
 ];

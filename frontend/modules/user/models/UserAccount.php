fa669c894f197d2afc1925bb1aa67e4d57b52e07
upliu <i@upliu.net>
2016-07-15 17:14:41 +0800

    修复 数据表前缀 bug

--- frontend/modules/user/models/UserAccount.php
+++ frontend/modules/user/models/UserAccount.php
@@ -22,7 +22,7 @@ class UserAccount extends \yii\db\ActiveRecord
      */
     public static function tableName()
     {
-        return 'user_account';
+        return '{{%user_account}}';
     }
 
     /**

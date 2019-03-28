24719580e5b4f7d0cb488e125cd5422b32c8e571
forecho <caizhenghai@gmail.com>
2019-03-28 18:42:10 +0800

    fix: install error

--- common/config/params.php
+++ common/config/params.php
@@ -13,4 +13,5 @@ return [
     'icon-framework' => 'fa',  // Font Awesome Icon framework
     'qrCodePath' => '/web/uploads/qr-code/',
     'qrCodeUrl' => '/uploads/qr-code/',
+    'newUserPostLimit' => 0, // 防止 spam，可限制新注册用户多少秒之后才能发帖，默认0代表不限制，单位是秒
 ];
--- frontend/config/params.php
+++ frontend/config/params.php
@@ -4,7 +4,6 @@ return [
     'donateNode' => ['tricks'], // 开启打赏分类
     'loginNode' => ['jobs'], // 需要登录才能访问的分类
     'donateTag' => ['求打赏', '技巧库'], // 开启标签
-    'newUserPostLimit' => 0, // 防止 spam，可限制新注册用户多少秒之后才能发帖，默认0代表不限制，单位是秒
     'setting' => [
         'xunsearch' => false, // true 表示开启 GetYii xunsearch 搜索功能，默认不开启
     ],
